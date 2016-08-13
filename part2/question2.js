924f4ad4daa6f48c00e5abd5f1c4ebf3b8489889
JameelB <c12456888@mydit.ie>
2016-08-13 02:06:25 +0100

    Added solution for question 2

--- part2/question2.js
+++ part2/question2.js
@@ -1,36 +1,46 @@
+const async = require('async');
+
 function remoteMathService (cb) {
-  var one;
-  var two;
+  var one = null;
+  var two = null;
 
-  callOneService( function (err, num) {
-    one = num;
-  });
-
-  callTwoService( function (err, num) {
-    two = num;
-  });
-
-  return cb( undefined , one + two);
+  async.parallel([
+    function (callback) {
+      callOneService( function (err, num) {
+      one = num;
+      callback();
+    });
+    },
+    function (callback) {
+      callTwoService( function (err, num) {
+        two = num;
+        callback();
+      });
+    },
+  ], function(err) {
+    if(err) return err;
+    return cb( undefined , one + two);
+  });
 }
 
 function callOneService (cb) {
   setTimeout ( function () {
-      return cb( undefined , 1 );
-    }, 1000 );
+      return cb( undefined , 1 );
+    }, 1000 );
 }
 
 function callTwoService (cb) {
   setTimeout ( function () {
-    return cb( undefined , 2 );
-  }, 1500 );
+    return cb( undefined , 2 );
+  }, 1500 );
 }
 
 remoteMathService( function (err, answer) {
-  if (err) console.log( "error " , err);
-  if (answer == 3 ) {
-    console.log ( "correct" );
+  if (err) console.log( "error " , err);
+  if (answer !== 3 ) {
+    console .log ( "wrong answer" , answer);
   }
   else {
-    console.log ("wrong answer" , answer);
+    console .log ( "correct" );
   }
-});
+});
