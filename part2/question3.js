974cf074762b2e22c30d2bc2069f762ba22a0242
JameelB <c12456888@mydit.ie>
2016-08-13 02:07:40 +0100

    Added solution for question 3

---
+++ part2/question3.js
@@ -0,0 +1,26 @@
+// This is a mock database implementation with just a connect function
+/* db.connect will need to be called a total of 10 times before it successfully
+connects */
+var counter = 0 ;
+var callback =  function (err) {
+  if (err) {
+    console.error(err);
+    return db.connect(callback);
+  }
+  console .log ( 'successfully connected!' );
+};
+
+var db = {
+  connect : function (cb) {
+    console .log ( 'connection attempt' , counter + 1 );
+    if (counter < 9 ) {
+      counter ++ ;
+      return cb( 'db not ready yet' );
+    }
+    return cb();
+  }
+};
+
+// Try to connect, log a successful connection & exit
+// If we fail to connect, log an error and return
+db.connect(callback);
