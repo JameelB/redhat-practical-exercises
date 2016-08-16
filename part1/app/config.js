9fa33627127403574ea79c83869f11a363d4fc76
JameelB <c12456888@mydit.ie>
2016-08-16 23:44:17 +0100

    Removed openshift variables

--- part1/app/config.js
+++ part1/app/config.js
@@ -1,13 +1,5 @@
 var connection_string = 'mongodb://localhost:27017/redhat';
 
-if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD){
-  connection_string = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
-  process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
-  process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
-  process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
-  process.env.OPENSHIFT_APP_NAME;
-}
-
 module.exports = {
 
     'mongo': connection_string
